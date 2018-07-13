fb16410fcadee629f39c3eced8bb18cbb2c2f3c0
ko3 <ek.lft15@gmail.com>
2018-07-13 17:31:55 -0500

    @Adding integration test

---
+++ src/test/java/com/monks/spring/controller/MemberControllerTest.java
@@ -0,0 +1,65 @@
+package com.monks.spring.controller;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.monks.spring.SpringIntegrationTestsApplication;
+import com.monks.spring.controller.requestmodel.Member;
+import com.monks.spring.integrationtestconfig.IntegrationTestContext;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringIntegrationTestsApplication.class)
+@ContextConfiguration(classes = IntegrationTestContext.class)
+@WebAppConfiguration
+public class MemberControllerTest {
+	
+	private MockMvc mockMvc;
+	
+	@Autowired
+    private WebApplicationContext webApplicationContext;
+	
+	@Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void test() throws Exception {
+		Member member = new Member();
+		member.setName("Rajinikanth");
+		member.setDescription("No one can stop");
+		
+		MvcResult mvcResult = mockMvc.perform(post("/member")
+												.contentType(MediaType.APPLICATION_JSON)
+												.content(asJsonString(member)))
+									.andExpect(status().isOk())
+									.andReturn();
+		System.out.println(mvcResult);
+	}
+	
+	public static String asJsonString(final Object obj) {
+	    try {
+	        return new ObjectMapper().writeValueAsString(obj);
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+	}
+
+}
